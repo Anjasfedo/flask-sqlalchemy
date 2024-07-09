2556aa3555821dcf921930b7d09f6b63a3e78689
AnjasFedo <fedoaf98@gmail.com>
2024-07-09 14:27:57 +0700

    with models.py

--- app.py
+++ app.py
@@ -2,23 +2,19 @@ from flask import Flask, request, render_template
 from flask_sqlalchemy import SQLAlchemy
 import os
 
+from models import db, User
+
 file_path = os.path.abspath(os.getcwd()) + "\\test.db"
 
 print("Database file path:", file_path)
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + file_path
-db = SQLAlchemy(app)
 
+db.init_app(app)
 
-
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(80), unique=True, nullable=False)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-
-    def __repr__(self):
-        return '<User %r>' % self.username
+with app.app_context():
+    db.create_all()
 
 @app.route("/", methods=["GET", "POST"])
 def hello_world():
@@ -39,7 +35,6 @@ def hello_world():
 
     return render_template("index.html", users=users)
 
+
 if __name__ == "__main__":
-    with app.app_context():
-        db.create_all()
-        app.run(debug=True)
+    app.run(debug=True)
